0e218ef32b3c8920ef9c41a4c670d6ae98d19fef
heregoesn0thing <heregoesnothing199@gmail.com>
2026-02-14 15:11:01 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const path = require("path")
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.static(path.join(__dirname)))
+
+io.on("connection", (socket) => {
+
+  console.log("User connected")
+
+  socket.on("crearAeronave", data => {
+    socket.broadcast.emit("crearAeronave", data)
+  })
+
+  socket.on("actualizarAeronave", data => {
+    socket.broadcast.emit("actualizarAeronave", data)
+  })
+
+  socket.on("eliminarAeronave", id => {
+    socket.broadcast.emit("eliminarAeronave", id)
+  })
+
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
